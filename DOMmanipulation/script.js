b5ac463ebb976c77f023b5ccb3cc8af84ec0dc2c
lucas <marcopolo1254rn@gmail.com>
2025-12-18 15:24:12 -0300

    Creating new structure to accept DOM manipulation content and practice

---
+++ DOMmanipulation/script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#bestDiv");
+
+const addingContent = document.createElement("div");
+addingContent.classList.add("content");
+addingContent.textContent = "This is just the beggining";
+
+container.appendChild(addingContent)
